2e28fc38441973bcc72f78b9c432f6daf7a0eefb
Harish Seenivasan <137220177+hvr1sh@users.noreply.github.com>
2023-10-21 13:19:02 -0500

    les go

--- bet.py
+++ bet.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
 
 st.title("Sports Betting")
 
@@ -28,7 +29,7 @@ def load_data(year):
     pbp = pd.DataFrame()
 
     for yr in year:
-        for week in weeks:
+        for week in tqdm(weeks, desc = 'fetching'):
             parameters = {"year":yr, "week":week}
             pbp_req = requests.get("https://api.collegefootballdata.com/games", 
                                 params = parameters, 
@@ -43,25 +44,61 @@ def load_data(year):
 
     
     pbp = pbp.query("away_conference in ('SEC', 'ACC', 'Big 12', 'Big Ten', 'Pac-12')")
+    pbp = pbp.query("away_conference in ('SEC', 'ACC', 'Big 12', 'Big Ten', 'Pac-12')")
+
     pbp = pbp.filter(items=['home_team', 'away_team', 'neutral_site', 'home_points', 'away_points'])
+
     return pbp
 
 trainingData = load_data([2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021])
 df_2022 = load_data([2022])
 
-
-st.table(trainingData.head())
-
-y_home_train = trainingData.home_points
-y_away_train = trainingData.away_points
+st.table(trainingData)
 
 #home field, weather, player quality, player health, starting qb rating, fpi data, power ranking,
 win_factors = ['home_team', 'away_team', 'neutral_site', 'home_points', 'away_points']
 X_train = trainingData[win_factors]
-X_test = df_2022['home_team', 'away_team', 'neutral_site', 'home_points', 'away_points']
+X_test = df_2022[win_factors]
+y_train_away = trainingData['away_points']
+y_test_away = df_2022['away_points']
 
-betting_model_home = DecisionTreeRegressor(random_state=1)
-betting_model_away = DecisionTreeRegressor(random_state=1)
 
-betting_model_home.fit(X_train, y_home_train)
-betting_model_away.fit(X_train, y_away_train)
+#One-Hot Encoding
+cat_cols = ['home_team', 'away_team']
+num_cols = ['home_points', 'away_points']
+bool_cols = ['neutral_site']
+
+categorical_transformer = Pipeline(steps=[
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+numeric_transformer = Pipeline(steps=[
+    ('scaler', StandardScaler())
+])
+
+    # For boolean features, you can apply no preprocessing, as they are already binary.
+boolean_transformer = "passthrough"
+
+preprocessor = ColumnTransformer(
+transformers=[
+    ('num', numeric_transformer, num_cols),
+    ('cat', categorical_transformer, cat_cols),
+    ('bool', boolean_transformer, bool_cols)
+])
+
+betting_model_away = RandomForestRegressor(n_estimators=100, random_state=42)
+
+regression_pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', betting_model_away)
+])
+
+regression_pipeline.fit(X_train, y_train_away)
+
+away_score_pred = regression_pipeline.predict(X_test)
+
+st.table(away_score_pred)
+
+mse = mean_squared_error(y_test_away, away_score_pred)
+
+print("Mean Squared Error: ", mse)
